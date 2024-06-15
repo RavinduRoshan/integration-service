1b1cd375c6cdb563b133a17967cfea9098210a61
unknown <ravinduroshan.rr@gmail.com>
2024-06-15 11:11:04 +0200

    item writer to write the summary to the output json file

---
+++ ohs-api-test/integration-service/src/main/java/org/vgcs/JsonFileWriter.java
@@ -0,0 +1,20 @@
+package org.vgcs;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.batch.item.Chunk;
+import org.springframework.batch.item.ItemWriter;
+import org.springframework.stereotype.Component;
+import org.vgcs.model.ProcessedOrder;
+
+import java.io.File;
+
+@Component
+public class JsonFileWriter implements ItemWriter<ProcessedOrder> {
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    @Override
+    public void write(Chunk<? extends ProcessedOrder> chunk) throws Exception {
+        System.out.println("WWWWWWWWWWWWWWWWWWWWWWWWWWWWWW");
+        objectMapper.writeValue(new File("processed-orders.json"), chunk);
+    }
+}
