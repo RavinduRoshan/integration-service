d0693226269bd09ea9402ea19891e42317a9b86d
unknown <ravinduroshan.rr@gmail.com>
2024-06-15 12:25:25 +0200

    refactor the code and use logger

--- ohs-api-test/integration-service/src/main/java/org/vgcs/service/OrderProcessor.java
+++ ohs-api-test/integration-service/src/main/java/org/vgcs/service/OrderProcessor.java
@@ -2,8 +2,11 @@ package org.vgcs.service;
 
 import com.google.protobuf.StringValue;
 
+import lombok.NonNull;
 import order.Order;
 import order.OrderServiceGrpc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
@@ -16,6 +19,7 @@ import java.util.concurrent.CompletableFuture;
 
 @Component
 public class OrderProcessor implements ItemProcessor<RowOrder, ProcessedOrder> {
+    private static final Logger LOGGER = LoggerFactory.getLogger(OrderProcessor.class);
 
     @Autowired
     private UserServiceGrpc.UserServiceBlockingStub userServiceStub;
@@ -23,11 +27,26 @@ public class OrderProcessor implements ItemProcessor<RowOrder, ProcessedOrder> {
     @Autowired
     private OrderServiceGrpc.OrderServiceBlockingStub orderServiceStub;
 
-
     @Override
-    public ProcessedOrder process(RowOrder newOrder) throws Exception {
-        System.out.println("OOOOOOOOOOOOOOOOrder : " + newOrder.getFullName());
-        CompletableFuture<User.UserResponse> userFuture = CompletableFuture.supplyAsync(() -> {
+    public ProcessedOrder process(@NonNull RowOrder newOrder) throws Exception {
+        CompletableFuture<User.UserResponse> userFuture = getUserFuture(newOrder);
+
+        User.UserResponse userResponse = userFuture.get();
+        String userPid = userResponse.getPid();
+        LOGGER.info("User created. userId: {}", userPid);
+
+        CompletableFuture<Order.OrderResponse> orderFuture = getOrderFuture(newOrder, userPid);
+
+        Order.OrderResponse orderResponse = orderFuture.get();
+        String orderPid = orderResponse.getPid();
+        String supplierPid = ""; // TODO: call supplier service to retrieve the supplier ID
+
+        LOGGER.info("Order created. orderId: {}", orderPid);
+        return new ProcessedOrder(userPid, orderPid, supplierPid);
+    }
+
+    private CompletableFuture<User.UserResponse> getUserFuture(RowOrder newOrder) {
+        return CompletableFuture.supplyAsync(() -> {
             User.CreateUserRequest userRequest = User.CreateUserRequest.newBuilder()
                     .setFullName(StringValue.of(newOrder.getFullName()))
                     .setEmail(newOrder.getEmail())
@@ -42,11 +61,10 @@ public class OrderProcessor implements ItemProcessor<RowOrder, ProcessedOrder> {
                     .build();
             return userServiceStub.createUser(userRequest);
         });
+    }
 
-        User.UserResponse userResponse = userFuture.get();
-        String userPid = userResponse.getPid();
-
-        CompletableFuture<Order.OrderResponse> orderFuture = CompletableFuture.supplyAsync(() -> {
+    private CompletableFuture<Order.OrderResponse> getOrderFuture(RowOrder newOrder, String userPid) {
+        return CompletableFuture.supplyAsync(() -> {
             Order.CreateOrderRequest orderRequest = Order.CreateOrderRequest.newBuilder()
                     .setUserPid(userPid)
                     .setDateCreated(StringValue.of(newOrder.getDateCreated().substring(0, 10)))
@@ -58,12 +76,6 @@ public class OrderProcessor implements ItemProcessor<RowOrder, ProcessedOrder> {
                     .build();
             return orderServiceStub.createOrder(orderRequest);
         });
-
-        Order.OrderResponse orderResponse = orderFuture.get();
-        String orderPid = orderResponse.getPid();
-        String supplierPid = "";
-
-        return new ProcessedOrder(userPid, orderPid, supplierPid);
     }
 }
 
