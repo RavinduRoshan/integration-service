f8913acd84f156bec3d3b3d97793a6913503aaab
unknown <ravinduroshan.rr@gmail.com>
2024-06-15 12:28:35 +0200

    read service ports from properties

--- ohs-api-test/integration-service/src/main/java/org/vgcs/config/GrpcConfig.java
+++ ohs-api-test/integration-service/src/main/java/org/vgcs/config/GrpcConfig.java
@@ -3,23 +3,32 @@ package org.vgcs.config;
 import io.grpc.ManagedChannel;
 import io.grpc.ManagedChannelBuilder;
 import order.OrderServiceGrpc;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import user.UserServiceGrpc;
 
 @Configuration
 public class GrpcConfig {
+    @Value("${service.url}")
+    private String url;
+
+    @Value("${user.service.port}")
+    private Integer userPort;
+
+    @Value("${order.service.port}")
+    private Integer orderPort;
 
     @Bean
     public ManagedChannel userServiceChannel() {
-        return ManagedChannelBuilder.forAddress("localhost", 50054)
+        return ManagedChannelBuilder.forAddress(url, userPort)
                 .usePlaintext()
                 .build();
     }
 
     @Bean
     public ManagedChannel orderServiceChannel() {
-        return ManagedChannelBuilder.forAddress("localhost", 50053)
+        return ManagedChannelBuilder.forAddress(url, orderPort)
                 .usePlaintext()
                 .build();
     }
